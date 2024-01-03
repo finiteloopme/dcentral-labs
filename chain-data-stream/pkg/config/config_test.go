1dd57bb65b31a5fc064cc040c3ed9ff0064c0a62
Kunal L <kunallimaye@gmail.com>
2024-01-04 09:18:37 +1100

    renaming file to config.go

---
+++ chain-data-stream/pkg/config/config_test.go
@@ -0,0 +1,18 @@
+package config_test
+
+import (
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/finiteloopme/dcentral-labs/chain-data-stream/pkg/config"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestLoad(t *testing.T) {
+	const testValue = "testing123"
+	os.Setenv("ETH_WEB_SOCKET_URL", testValue)
+	v := &config.Config{}
+	v.Load()
+	assert.EqualValues(t, testValue, v.EthWebSocketUrl, fmt.Sprintf("Expected %s, received %s", testValue, v.EthWebSocketUrl))
+}
